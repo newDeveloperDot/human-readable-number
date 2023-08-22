9dd0b70c0b5453bcb3993d4bb655284a4b0f1529
newDeveloperDot <82229807+newDeveloperDot@users.noreply.github.com>
2023-08-22 12:51:05 +0500

    feat: set to read the given number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+  if (number === 0) return "zero";
+
+  const hundred = Math.floor(number / 100);
+  const numberDecimal = number % 100;
+
+  const decimal = Math.floor(numberDecimal / 10);
+  const unit = numberDecimal % 10;
+
+  const units = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+  const unitsTeen = ["", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const decimals = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  let result = "";
+  if (hundred > 0) result += units[hundred] + " hundred ";
+
+  if (numberDecimal > 10 && numberDecimal < 20) result += unitsTeen[unit];
+  else {
+    if (decimal > 0) result += decimals[decimal] + " ";
+    if (unit > 0) result += units[unit];
+  }
+
+  return result.trim();
 }
